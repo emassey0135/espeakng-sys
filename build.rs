bbd65fa6c30098d5fa4c6883d4d5f9692ccf1c3b
Elijah Massey <emassey0135@gmail.com>
2025-01-26 22:22:48 -0600

    Add cmake options for eSpeak-NG

--- build.rs
+++ build.rs
@@ -6,6 +6,19 @@ use cmake::Config;
 
 fn main() {
     let dst = Config::new("espeak-ng")
+      .define("BUILD_SHARED_LIBS", "OFF")
+      .define("COMPILE_INTONATIONS", "ON")
+      .define("ENABLE_TESTS", "ON")
+      .define("ESPEAK_BUILD_MANPAGES", "OFF")
+      .define("EXTRA_cmn", "ON")
+      .define("EXTRA_ru", "ON")
+      .define("EXTRA_yue", "ON")
+      .define("USE_ASYNC", "ON")
+      .define("USE_MBROLA", "OFF")
+      .define("USE_LIBSONIC", "ON")
+      .define("USE_LIBPCAUDIO", "OFF")
+      .define("USE_KLATT", "ON")
+      .define("USE_SPEECHPLAYER", "ON")
       .build();
     println!("cargo:rustc-link-search=native={}", dst.join("lib").display());
     println!("cargo:rustc-link-lib=static=espeak-ng");
